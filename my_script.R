9e937e894e92262e5c3e21404c454afdbabbdcfa
Nicholas Harrigan <nickharrigan@gmail.com>
2019-07-16 13:47:34 +1000

    832 - hide as drafts all but unit guide

--- my_script.R
+++ my_script.R
@@ -12,6 +12,35 @@ library(rmarkdown)
 
 setwd("c:/users/nickh/methods101/")
 blogdown::serve_site()
+browseURL("http://127.0.0.1:4321/docs/")
+
+?datasets
+
+x<-1
+age <- c(36, 19, 30, 55, 42)
+gender <- c("male", "female", "female", "male", "female")
+ds <- cbind(age, gender)
+ds <- as.data.frame(data)
+
+length(x)
+str(x)
+class(x)
+names(x)
+
+length(age)
+str(age)
+class(age)
+names(age)
+
+length(gender)
+str(gender)
+class(gender)
+names(gender)
+
+length(ds)
+str(ds)
+class(ds)
+names(ds)
 
 
 
